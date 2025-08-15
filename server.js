f749ba75ecb260eef57d39e6bcf4464ba5cced08
Nguyen Thien An <nguyenthienandzpro@gmail.com>
2025-08-15 21:02:32 +0700

    import three library morgan,helmet and compression

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require("./src/app");
+const PORT = 3000;
+const server = app.listen(PORT, () => {
+  console.log(`PixelSmith server start with port ${PORT}`);
+});
+
+// process.on("SIGINT", () => {
+//   server.close(() => console.log(`Exit PixelSmith Server Express`));
+//   //notify.send(ping...)
+// });
---
+++ src/app.js
@@ -0,0 +1,25 @@
+const compression = require("compression");
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression())
+
+// init db
+
+// init routes
+app.get("/", (req, res, next) => {
+//   const stringCompress = "Nguyễn Thiên Ân";
+  return res.status(200).json({
+    message: "Welcome PixelSmith",
+    // metadata: stringCompress.repeat(10000),
+  });
+});
+
+// handling error
+
+module.exports = app;
